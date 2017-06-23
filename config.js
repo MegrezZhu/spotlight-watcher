93d8577e73f837401fd5f3795f42d4a07c4daca5
Zhu Chu <mystery490815101@gmail.com>
2017-06-23 15:42:13 +0800

    directory changed

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
   imageDir: '%localappdata%/Packages/Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy/LocalState/Assets',
   username: 'i4908',
-  targetDir: 'C:/Users/i4908/Desktop/pics'
+  targetDir: 'D:/WindowsSpotlight'
 };
